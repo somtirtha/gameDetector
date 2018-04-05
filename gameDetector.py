e10039a91114cbb7a21acb9f5ca09cfd2f091ee3
Somtirtha Roy <somtirtha.roy@gmail.com>
2018-04-05 14:05:05 -0600

    wrote n^3 algorithm to tag game tags in corpus, reduced the complexity to n^2 by preprocessing the ngramMap

--- gameDetector.py
+++ gameDetector.py
@@ -1,17 +1,77 @@
-
+import operator
 
 
 class GameDetector:
 
     def __init__(self):
-        pass
+        self.processedMapList = []
 
-    def getTaggedCorpus(self, ngramMap, corpus):
-        return []
+    def processMapList(self, ngramMap):
+        for key, items in ngramMap.items():
+            for item in items:
+               self.processedMapList.append((key, item, len(item.split(" ")))) 
+
+        self.processedMapList.sort(key=operator.itemgetter(2), reverse=True)
 
 
+    def findNgrams(self, inputList, n):
+        """
 
+        """
+        return zip(*[inputList[i:] for i in range(n)])
 
+    def cleanEntry(self, entry):
+        """
+
+        """
+        entry = entry.lower()
+        entryList = entry.split(" " )
+        return entryList
+
+    def getTaggedCorpus(self, ngramMap={}, corpus=[]):
+        """
+
+        """
+
+        # process map before search
+        self.processMapList(ngramMap)
+
+        outputList = []
+        # detect ngrams from corpus
+        for entry in corpus:
+            # clean entry
+            cleanList = self.cleanEntry(entry)
+            
+            idTokenList = []
+            for id, token, tokenLength in self.processedMapList:
+            # for id, tokenList in ngramMap.items():
+                # for token in tokenList:
+                    #print(token)
+
+                # find Ngrams depending on the length of the token
+                ngrams = list( self.findNgrams(cleanList, tokenLength) )
+
+                # convert token to lower case, splut into words and convert it into a tuple
+                tokenTuple = tuple(token.lower().split(" "))
+
+                # tuple present in ngrams append the id and toekn into a list
+                if tokenTuple in ngrams:
+                    idTokenList.append((id, token))
+            
+            # replace and append to output list here
+            for id, token in idTokenList:
+                entry = entry.replace(token, "TAG{" + id + "," + token + "}")
+            outputList.append(entry)
+
+        return outputList
+
+"""
+eg o/p:
+[ "I liked TAG{Destiny2,the last Destiny game}, now I play TAG{Fortnite,Fortnite}" ,
+"Lol, no comment about that" ,
+...,
+"I'm still playing TAG{WorldOfWarcraft,world of warcraft} since ww2" ]
+"""
 
 
 if __name__ == '__main__':
@@ -23,13 +83,20 @@ if __name__ == '__main__':
                     "WorldOfWarcraft": [ "WoW the game" , "world of warcraft" ]
                 }
 
+    ngramMap2 = {
+                    "Fortnite": [ "Fortnite" , "Fort Nite" ],
+                    "Destiny2": [ "Destiny 2" , "the last Destiny game" , "Destiny II" ],
+                }
+
 
     corpus = ["I liked the last Destiny game, now I play Fortnite", \
               "Lol, no comment about that" , \
               "I'm still playing world of warcraft since ww2"]
 
+    corpus = ["I liked the last Destiny game, now I play Fortnite"]
+
     gDetector = GameDetector()
     tagCorpus = gDetector.getTaggedCorpus(ngramMap, corpus)
-    print tagCorpus
+    print(tagCorpus)
 
 
--- testGameDetector.py
+++ testGameDetector.py
@@ -1,4 +1,5 @@
 import unittest2 as unittest
+import gameDetector as gd
 
 
 
@@ -8,6 +9,18 @@ class TestGameDetector(unittest.TestCase):
     def test(self):
         self.assertEqual(3, 3)
 
+
+    def testOutput(self):
+        gameD = gd.GameDetector()
+        self.assertEqual(gameD.getTaggedCorpus(), [])
+
+    def testTuple(self):
+        t1= [1,2,3,4]
+        tList = [(2,3,4), (1,2,3,4),(3,4,5,6)]
+        if tuple(t1) in tList:
+            self.assertEqual(3, 3)
+        else:
+            self.assertEqual(3, 4)
 
 if __name__ == '__main__':
     unittest.main()
